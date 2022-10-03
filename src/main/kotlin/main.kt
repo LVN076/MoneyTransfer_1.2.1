161d999b9598f2b4e890d6f07af399ba364dc393
Лисин Виталий <lega997@gmail.com>
2022-10-03 23:45:32 +0300

    добавлены все файлы проекта

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+package ru.netology
+
+fun main () {
+    val mount = 100
+    var comission = 0.0075 * mount
+    if (comission <=35) {
+        println("Ваша комисиия 35 рублей")
+    }else {
+        println("Ваша комиссия $comission рублей")
+    }
+
+
+
+
+}
+
+
